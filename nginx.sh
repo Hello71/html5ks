c27f033f4d794e28f4a6939df87ba54336127af0
Alex Xu <alex_y_xu@yahoo.ca>
2013-07-27 14:47:45 -0400

    use gzip_static only if nginx supports it

--- nginx.sh
+++ nginx.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec nginx -p `dirname $0` -c nginx.conf
+cd "`dirname $0`"
+touch nginx.gen.conf
+if nginx -V 2>&1 | grep -q -- --with-http_gzip_static_module; then
+  echo "gzip_static on" >> nginx.gen.conf
+fi
+exec nginx -p "$PWD" -c nginx.conf
