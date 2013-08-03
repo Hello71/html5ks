e3e9a0947a2b04348cc9b77aff054960485b4393
Alex Xu <alex_y_xu@yahoo.ca>
2013-08-03 12:46:16 -0400

    nginx.sh: don't keep appending to nginx.gen.conf

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd "`dirname $0`"
-touch nginx.gen.conf
+# empty file
+> nginx.gen.conf
 if nginx -V 2>&1 | grep -q -- --with-http_gzip_static_module; then
   echo "gzip_static on" >> nginx.gen.conf
 fi
