ce961bb5865b5d16d203365159ab173e67e34556
Alex Xu <alex_y_xu@yahoo.ca>
2013-08-18 12:09:30 -0400

    fix nginx.* to work better with slim nginx
    also print warning if zopfli is not found

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,36 @@
-#!/bin/sh
-cd "`dirname $0`"
+#!/bin/bash
+
+set -e
+cd "$(dirname $0)"
+
+
 # empty file
 > nginx.gen.conf
-if nginx -V 2>&1 | grep -q -- --with-http_gzip_static_module; then
-  echo "gzip_static on" >> nginx.gen.conf
+
+
+V=$(nginx -V 2>&1)
+
+if echo ${V} | grep -q -- --with-http_gzip_static_module; then
+  echo "gzip_static on;" >> nginx.gen.conf
+else
+  echo >&2 "The gzip_static module for nginx is highly recommended to reduce server load and utilize zopfli's higher compression ratio."
 fi
+
+if ! echo ${V} | grep -q -- --without-http_proxy_module; then
+  echo "proxy_temp_path /dev/null;" >> nginx.gen.conf
+fi
+
+if ! echo ${V} | grep -q -- --without-http_fastcgi_module; then
+  echo "fastcgi_temp_path /dev/null;" >> nginx.gen.conf
+fi
+
+if ! echo ${V} | grep -q -- --without-http_scgi_module; then
+  echo "scgi_temp_path /dev/null;" >> nginx.gen.conf
+fi
+
+if ! echo ${V} | grep -q -- --without-http_uwsgi_module; then
+  echo "uwsgi_temp_path /dev/null;" >> nginx.gen.conf
+fi
+
+
 exec nginx -p "$PWD" -c nginx.conf
--- unrpyc/find-gzip.sh
+++ unrpyc/find-gzip.sh
@@ -4,7 +4,11 @@ GZIP="$(command -v gzip)"
 if [ -n $ZOPFLI ]; then
   echo "$ZOPFLI"
 elif [ -n $GZIP ]; then
+  echo >&2 "Consider obtaining zopfli (https://code.google.com/p/zopfli/)"
+  echo >&2 "for higher compression ratios (about 5% decrease in size of"
+  echo >&2 "script.json.gz."
   echo "$GZIP -9"
 else
+  echo >&2 "Could not find zopfli or gzip, aborting."
   exit 1
 fi
