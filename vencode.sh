a8fc940d0dd6736a1098fb064949d0531be6b586
Alex Xu <alex_y_xu@yahoo.ca>
2013-09-02 14:34:17 -0400

    Revert "never overwrite"
    This reverts commit 6731d516f79ec5c54fae76d557ec512fd952a7c8.

--- vencode.sh
+++ vencode.sh
@@ -11,13 +11,13 @@ cd $(dirname $0)/www/dump/video
 
 ffmpeg() {
   set -x
-  command ${FFMPEG} -threads ${THREADS} ${FFMPEG_FLAGS} "$@"
+  command ${FFMPEG} -n -threads ${THREADS} ${FFMPEG_FLAGS} "$@"
   set +x
 }
 
 for f in *.mkv; do
   OUT=${f%.mkv}
-  [[ -f ${OUT}.mp4 ]] || ffmpeg -i $f -c:v libx264 -preset slower -tune animation -c:a libfdk_aac ${OUT}.mp4
-  [[ -f ${OUT}.webm ]] || ffmpeg -i $f -c:v libvpx -crf 15 -b:v 1M -c:a copy ${OUT}.webm
-  [[ -f ${OUT}.ogv ]] || ffmpeg -i $f -c:v libtheora -qscale:v 6 -c:a copy ${OUT}.ogv
+  ffmpeg -i $f -c:v libx264 -preset slower -tune animation -c:a libfdk_aac ${OUT}.mp4
+  ffmpeg -i $f -c:v libvpx -crf 15 -b:v 1M -c:a copy ${OUT}.webm
+  ffmpeg -i $f -c:v libtheora -qscale:v 6 -c:a copy ${OUT}.ogv
 done
