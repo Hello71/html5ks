e01fd685f236bbe131734af6be0f340cde79de94
Alex Xu <alex_y_xu@yahoo.ca>
2014-01-08 14:19:11 -0500

    Move trim to Makefile

--- setup.sh
+++ setup.sh
@@ -7,16 +7,7 @@ cd $(dirname $0)
 git submodule update --init
 
 pushd unrpyc
-make install
+make ${MAKEOPTS} install
 popd
-
-trim() {
-  for f in "$@"; do
-    convert -trim "$f" "$f"
-  done
-  optipng -o7 "$@"
-}
-
-trim www/dump/ui/bt-cf-unchecked.png www/dump/ui/bt-cf-checked.png
 
 make ${MAKEOPTS}
