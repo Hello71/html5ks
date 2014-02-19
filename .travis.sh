e753bebcce09f1b2d3edd99c3cd1b2a3487ceda9
Alex Xu <alex_y_xu@yahoo.ca>
2014-02-19 09:45:17 -0500

    asdf

--- .travis.sh
+++ .travis.sh
@@ -2,26 +2,28 @@
 
 set -e -x
 
-MAKEOPTS="-j`nproc`"
+uname -a
 
 case "$1" in
   before_install)
     sudo apt-get update -q
 
-    curl http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz | tar -xz
+    curl http://nodejs.org/dist/node-latest.tar.gz | tar -xz
     curl -L http://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-0.1.3.tar.gz | tar -xz
     curl http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz | tar -xz
     curl http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 | tar -xj
     curl https://webp.googlecode.com/files/libwebp-0.4.0.tar.gz | tar -xz
 
-    curl http://dl.katawa-shoujo.com/gold_1.1/%5B4ls%5D_katawa_shoujo_1.1-%5Blinux-x86%5D%5BB5C707D5%5D.tar.bz2 | tar -xj --strip-components 2 --wildcards "Katawa Shoujo-linux-x86/game/script-a*" "Katawa Shoujo-linux-x86/game/imachine.rpyc" "Katawa Shoujo-linux-x86/game/ui-strings.rpyc" "Katawa Shoujo-linux-x86/game/ui-strings_FR.rpyc" "Katawa Shoujo-linux-x86/game/data.rpa"
+    curl http://dl.katawa-shoujo.com/gold_1.1/%5B4ls%5D_katawa_shoujo_1.1-%5Blinux-x86%5D%5BB5C707D5%5D.tar.bz2 | tar -xj --strip-components 2 --wildcards "Katawa Shoujo-linux-x86/game/script-a*.rpyc" "Katawa Shoujo-linux-x86/game/imachine.rpyc" "Katawa Shoujo-linux-x86/game/ui-strings*.rpyc" "Katawa Shoujo-linux-x86/game/data.rpa"
     mv *.rpyc unrpyc
     ;;
   install)
-    sudo apt-get install -q libjpeg-progs libtheora-dev libvpx-dev libx264-dev webp yasm
+    MAKEOPTS="-j`nproc`"
 
-    cd node-v0.10.26
-    ./configure
+    sudo apt-get install -q libtheora-dev libvpx-dev libx264-dev yasm
+
+    cd node-v*
+    ./configure --without-ssl
     make $MAKEOPTS
     sudo make install
     cd ..
@@ -29,13 +31,13 @@ case "$1" in
     sudo npm install -g uglify-js
 
     cd fdk-aac-0.1.3
-    ./configure --disable-shared
+    ./configure --disable-shared --disable-dependency-tracking
     make $MAKEOPTS
     sudo make install
     cd ..
 
     cd opus-1.1
-    ./configure --disable-shared
+    ./configure --disable-shared --disable-extra-programs --disable-dependency-tracking
     make $MAKEOPTS
     sudo make install
     cd ..
@@ -54,9 +56,6 @@ case "$1" in
 
     curl https://raw.github.com/Lattyware/unrpa/master/unrpa | python2 - -p www/dump -m data.rpa
     rm data.rpa
-    ;;
-  script)
-    exec ./setup.sh "$@"
     ;;
   *)
     exit 1
