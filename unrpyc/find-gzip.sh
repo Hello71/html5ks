ced7ede3cd0853c9954bda518aaf546d3a0e5389
Alex Xu <alex_y_xu@yahoo.ca>
2014-02-24 15:52:23 -0500

    obsolete setup.sh

--- unrpyc/find-gzip.sh
+++ unrpyc/find-gzip.sh
@@ -2,9 +2,8 @@
 if command -v zopfli; then
   :
 elif command -v gzip; then
-  echo >&2 "Consider obtaining zopfli (https://code.google.com/p/zopfli/)"
-  echo >&2 "for higher compression ratios (about 5% decrease in size of"
-  echo >&2 "script.json.gz)."
+  echo >&2 "Consider obtaining zopfli (https://github.com/Hello71/zopfli) for higher"
+  echo >&2 "compression ratios (about 5% decrease in size of script.json.gz)."
 else
   echo >&2 "Could not find zopfli or gzip, aborting."
   exit 1
