216de545b8bc079156c900b313d2bff2a211bdf3
Alex Xu <alex_y_xu@yahoo.ca>
2014-03-04 16:10:02 -0500

    tweak travis, lessen nginx restrictions

--- .travis.sh
+++ .travis.sh
@@ -16,16 +16,16 @@ case "$1" in
     curl http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 | tar -xj
     curl -L http://downloads.sourceforge.net/project/apngasm/2.7/apngasm-2.7-src.zip -O
     unzip apngasm-2.7-src.zip apngasm.c
+    rm apngasm-2.7-src.zip
 
     curl http://dl.katawa-shoujo.com/gold_1.1/%5B4ls%5D_katawa_shoujo_1.1-%5Blinux-x86%5D%5BB5C707D5%5D.tar.bz2 | tar -xj --strip-components 2 --wildcards "Katawa Shoujo-linux-x86/game/script-a*.rpyc" "Katawa Shoujo-linux-x86/game/imachine.rpyc" "Katawa Shoujo-linux-x86/game/ui-strings*.rpyc" "Katawa Shoujo-linux-x86/game/data.rpa"
     mv *.rpyc unrpyc
 
     npm config set tmp /tmp
-
     ;;
   install)
     MAKEOPTS="-s ${MAKEOPTS}"
-    sudo apt-get install -qq libtheora-dev libx264-dev yasm
+    sudo apt-get install -qq libtheora-dev libx264-dev nginx yasm
 
     cd libwebp-0.4.0
     sed -i -e '/unset ac_cv_header_GL_glut_h/d' configure
@@ -36,12 +36,14 @@ case "$1" in
     make $MAKEOPTS >/dev/null
     sudo make $MAKEOPTS install >/dev/null
     cd ..
+    rm -r libwebp-0.4.0
 
     cd node-v*
     ./configure --openssl-use-sys --shared-zlib
     make $MAKEOPTS >/dev/null
     sudo python tools/install.py install >/dev/null
     cd ..
+    rm -r node-v*
 
     sudo npm install -g uglify-js
 
@@ -50,18 +52,28 @@ case "$1" in
     make $MAKEOPTS >/dev/null
     sudo make $MAKEOPTS install >/dev/null
     cd ..
+    rm -r fdk-aac-0.1.3
 
     cd opus-1.1
     ./configure --disable-shared --disable-extra-programs --disable-dependency-tracking --quiet
     make $MAKEOPTS >/dev/null
     sudo make $MAKEOPTS install >/dev/null
     cd ..
+    rm -r opus-1.1
 
     cd libvpx-v1.3.0
     ./configure
     make $MAKEOPTS >/dev/null
     sudo make $MAKEOPTS install >/dev/null
     cd ..
+    rm -r libvpx-v1.3.0
+
+    cd ffmpeg
+    ./configure --disable-everything --disable-logging --disable-programs --disable-runtime-cpudetect --enable-decoder=mpeg4 --enable-decoder=pcm_s16le --enable-decoder=vorbis --enable-decoder=rawvideo --enable-demuxer=matroska --enable-demuxer=ogg --enable-demuxer=wav --enable-demuxer=yuv4mpegpipe --enable-encoder=libfdk_aac --enable-encoder=libopus --enable-encoder=libtheora --enable-encoder=libvpx_vp8 --enable-encoder=libvpx_vp9 --enable-encoder=libx264 --enable-encoder=pcm_s16le --enable-encoder=rawvideo --enable-ffmpeg --enable-filter=aresample --enable-gpl --enable-hardcoded-tables --enable-libfdk_aac --enable-libopus --enable-libtheora --enable-libvpx --enable-libx264 --enable-muxer=ipod --enable-muxer=mp4 --enable-muxer=ogg --enable-muxer=wav --enable-muxer=webm --enable-muxer=yuv4mpegpipe --enable-nonfree --enable-protocol=file
+    make $MAKEOPTS >/dev/null
+    sudo make $MAKEOPTS install >/dev/null
+    cd ..
+    rm -r ffmpeg
 
     patch -p0 <<EOF
 --- apngasm.c
@@ -77,18 +89,16 @@ case "$1" in
 EOF
     make LDLIBS="$(pkg-config --libs libpng --libs zlib)" apngasm
     sudo install -c -m755 apngasm /usr/local/bin
-
-    cd ffmpeg
-    ./configure --disable-everything --disable-logging --disable-programs --disable-runtime-cpudetect --enable-decoder=mpeg4 --enable-decoder=pcm_s16le --enable-decoder=vorbis --enable-decoder=rawvideo --enable-demuxer=matroska --enable-demuxer=ogg --enable-demuxer=wav --enable-demuxer=yuv4mpegpipe --enable-encoder=libfdk_aac --enable-encoder=libopus --enable-encoder=libtheora --enable-encoder=libvpx_vp8 --enable-encoder=libvpx_vp9 --enable-encoder=libx264 --enable-encoder=pcm_s16le --enable-encoder=rawvideo --enable-ffmpeg --enable-filter=aresample --enable-gpl --enable-hardcoded-tables --enable-libfdk_aac --enable-libopus --enable-libtheora --enable-libvpx --enable-libx264 --enable-muxer=ipod --enable-muxer=mp4 --enable-muxer=ogg --enable-muxer=wav --enable-muxer=webm --enable-muxer=yuv4mpegpipe --enable-nonfree --enable-protocol=file
-    make $MAKEOPTS >/dev/null
-    sudo make $MAKEOPTS install >/dev/null
-    cd ..
+    rm apngasm apngasm.c
 
     curl https://raw.github.com/Lattyware/unrpa/master/unrpa | python2 - -p www/dump -m data.rpa
     rm data.rpa
     ;;
   script)
-    exec make $MAKEOPTS
+    make $MAKEOPTS
+    ./nginx.sh
+    curl http://localhost:8080/
+    curl "http://localhost:8080/?warned=1"
     ;;
   *)
     exit 1
